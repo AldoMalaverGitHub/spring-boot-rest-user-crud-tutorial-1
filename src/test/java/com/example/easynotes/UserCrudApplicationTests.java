e6c0006d25c4b0f0d966a694286fa63f4988a607
Orlando Marcos Torres <omarcost@gmail.com>
2019-10-08 15:49:28 -0500

    first commit

---
+++ src/test/java/com/example/easynotes/UserCrudApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.easynotes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserCrudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
